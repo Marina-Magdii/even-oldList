61e63f190b799d911783b0ba8a50bf32575b1f19
Marina Magdy <marina.magdy.ibrahim97@gmail.com>
2025-01-13 21:23:17 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main (){
+  List <int> numbers =[32,45,42,12,64,8,9,2,1];
+  for (int num in numbers){
+    switch (num%2){
+      case 0:
+        print("This is and even number");
+      case 1:
+        print("This is an odd number");
+    }
+  }
+}
