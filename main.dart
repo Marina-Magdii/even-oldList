f2cfd0946431b8b75890ef25d5f757d3923f1254
Marina Magdy <marina.magdy.ibrahim97@gmail.com>
2025-01-13 21:26:25 +0200

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,6 +1,8 @@
 void main (){
+  // create a random list
   List <int> numbers =[32,45,42,12,64,8,9,2,1];
   for (int num in numbers){
+    // check if the division by 2 has remaining  or not
     switch (num%2){
       case 0:
         print("This is and even number");
